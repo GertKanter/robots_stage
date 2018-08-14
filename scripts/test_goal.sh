8d92df9dcf1b7790abf80e77c4b0f50e0764c577
Gert Kanter <gert.kanter@ttu.ee>
2018-08-14 08:27:23 +0000

    two test goals

--- scripts/test_goal.sh
+++ scripts/test_goal.sh
@@ -1,1 +1,1 @@
-rostopic pub /robot_0/move_base_simple/goal geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "/map"}, pose: {position: {x: 1.0, y: 0.0, z: 0.0}, orientation: {w: 1.0}}}'
+rostopic pub /robot_0/move_base_simple/goal geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "/map"}, pose: {position: {x: 3.0, y: 3.0, z: 0.0}, orientation: {w: 1.0}}}'
---
+++ scripts/test_goal2.sh
@@ -0,0 +1,1 @@
+rostopic pub /robot_0/move_base_simple/goal geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "/map"}, pose: {position: {x: 17.0, y: 3.0, z: 0.0}, orientation: {w: 1.0}}}'
